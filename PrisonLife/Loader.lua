c8b98bd0bc59e24c320dc1b04d77f94b0c191f2a
NoobZ <152039454+Noob-With-Z@users.noreply.github.com>
2025-11-11 14:31:44 -0300

    no more PrisonLife

--- PrisonLife/Loader.lua
+++
@@ -1,7 +0,0 @@
-local success, result = pcall(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Noob-With-Z/Games/refs/heads/main/PrisonLife/Main.lua"))()
-end)
-
-if not success then
-	warn("// Error while loading script: \n" .. result .. "\nIf it keeps to happen contact the developer of script.")
-end
